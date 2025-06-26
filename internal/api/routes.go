6688f6fdd28f20a8867af4dff0f175f847f9d9f8
shaibriz <97502054+shaibriz@users.noreply.github.com>
2025-06-26 08:24:20 +0545

    feat: refactor server and API handlers to use AleoPublicContext for improved context management

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -4,23 +4,21 @@ import (
 	"net/http"
 
 	"github.com/venture23-aleo/aleo-oracle-notarization-backend/internal/api/handlers"
-
-	aleo "github.com/zkportal/aleo-utils-go"
+	"github.com/venture23-aleo/aleo-oracle-notarization-backend/internal/services"
 )
 
 // RegisterRoutes registers the routes for the API.
-func RegisterRoutes(mux *http.ServeMux, s aleo.Session) {
+func RegisterRoutes(mux *http.ServeMux, aleoCtx services.AleoPublicContext) {
 	// Register the routes.
 
 	// Register the notarization route.
-	mux.HandleFunc("/notarize", handlers.GenerateAttestationReportHandler(s))
+	mux.HandleFunc("/notarize", handlers.GenerateAttestationReportHandler(aleoCtx))
 
 	// Register the info route.
-	mux.HandleFunc("/info", handlers.GetInfo)
+	mux.HandleFunc("/info", handlers.GetEnclaveInfo(aleoCtx))
 
 	// Register the whitelist route.
 	mux.HandleFunc("/whitelist", handlers.GetWhiteListedDomains)
-
 
 	mux.HandleFunc("/", handlers.GetHealthCheck)
 }
